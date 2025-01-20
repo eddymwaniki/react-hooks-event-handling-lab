b81f106bda3f2a4c7b1d4eb8e0feb3810e80e858
eddy <eddy.mwaniki.27@gmail.com>
2025-01-20 11:12:46 +0300

    modifies Keypad.js

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function focusHere(){
+        console.log("Good!")
+    }
+    function blurredAway(){
+        console.log("Hey! Eyes on me!")
+    }   
+    return <button onFocus={focusHere} onBlur={blurredAway}>Eyes on me</button>
+}
+
+export default EyesOnMe;
